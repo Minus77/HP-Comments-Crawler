cdec99c1b1083242a71af0b6be016318d61ea2d2
Lewis Wu <minus-@outlook.com>
2017-12-14 14:19:49 +0800

    1.1 version 添加评论类型选择，添加csv标题

--- main.py
+++ main.py
@@ -5,32 +5,38 @@ import requests
 import json
 import codecs
 
+print("请输入产品ID")
+productId=input()
+print("请输入所需评论种类\n0为全部评论、1为差评、2为好评")
+kindNum=input()
 s = requests.session()
-url = 'https://club.jd.com/comment/productPageComments.action'
+url = 'https://club.jd.com/comment/skuProductPageComments.action'
 data = {
-    'callback':'fetchJSON_comment98vv61',
-    'productId':'971916',
-    'score':1,
-    'sortType':5,
-    'pageSize':10,
-    'isShadowSku':0,
-    'page':0
+    'callback' : 'fetchJSON_comment98vv1941',
+    'productId' : productId,
+    'score' : kindNum,
+    'sortType' : 5,
+    'pageSize' : 10,
+    'isShadowSku' : 0,
+    'page' : 0,
+    'fold' : 1
 }
-
-myFile=codecs.open("test.csv" ,"w",encoding="utf_8_sig" )
+print("正在爬取评论数据")
+myFile=codecs.open(productId+".csv" ,"w",encoding="utf_8_sig" )
 myWriter=csv.writer(myFile,dialect='excel')
-
+title=['用户名','评论时间','用户终端','评论内容']
+myWriter.writerow(title)
 #    while i<10:
 #        myWriter.writerow([7,'g'])
 #        i=i+1
 #    myWriter.writerow([8,'h'])
 #    myList=[[1,2,3],[4,5,6]]
 #    myWriter.writerow(myList)
-for i in range(1):
+for i in range(100):
         t = s.get(url,params = data).text
 
         try:
-            t = re.search(r'(?<=fetchJSON_comment98vv61\().*(?=\);)',t).group(0)
+            t = re.search(r'(?<=fetchJSON_comment98vv1941\().*(?=\);)',t).group(0)
         except Exception as e:
             break
 
@@ -49,5 +55,7 @@ for i in range(1):
             myWriter.writerow(info)
             #print('{}  {}  {}\n{}\n'.format(c_name,c_time,c_client,c_content))
         data['page'] += 1
-
+print("数据抓取完成")
+while(0):
+    stop=input()
 myFile.close()
